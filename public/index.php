ae4c977204737c0cb623cf4176b58c5f634ae2bd
jonmilsom <j.milsom@pitchero.com>
2020-04-21 22:35:52 +0100

    Move index.php to public directory

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('storage/logs/app.log', Monolog\Logger::INFO));
+$log->pushHandler(new Monolog\Handler\StreamHandler('../storage/logs/app.log', Monolog\Logger::INFO));
 $log->addInfo($_SERVER['REMOTE_ADDR'] . ' ' . gethostname());
 
 echo '<h1>Hello World</h1>';
