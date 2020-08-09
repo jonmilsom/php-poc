560377591edd3d8ce1cc48ab56b8aa4578c2ad27
jonmilsom <jon@altstudio.co.uk>
2020-08-09 21:59:58 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('../storage/logs/app.log', Monolog\Logger::INFO));
 $log->addInfo($_SERVER['REMOTE_ADDR'] . ' ' . gethostname());
 
-echo '<h1>Hello World</h1>';
+echo '<h1>Hello World - And one more thing...</h1>';
 echo '<pre>' . print_r($log, true) . '</pre>';
