53ebcd37495cda278b87b43fa2505db4ddde3aa9
jonmilsom <jonmilsom@users.noreply.github.com>
2020-08-09 21:00:06 +0000

    Apply fixes from StyleCI

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__.'/../vendor/autoload.php';
 
 $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('../storage/logs/app.log', Monolog\Logger::INFO));
-$log->addInfo($_SERVER['REMOTE_ADDR'] . ' ' . gethostname());
+$log->addInfo($_SERVER['REMOTE_ADDR'].' '.gethostname());
 
 echo '<h1>Hello World - And one more thing...</h1>';
-echo '<pre>' . print_r($log, true) . '</pre>';
+echo '<pre>'.print_r($log, true).'</pre>';
